e8ec3ef3a03ce9cd8b1c78b28dfc853c19c634fb
MarcosRC-Santos <105668624+MarcosRC-Santos@users.noreply.github.com>
2022-05-16 15:47:21 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Guitarra minhaGuitarra = new Guitarra("01020304", "fender", "telecaster", "elétrica", "mogno", 1500);
+    }
+}
